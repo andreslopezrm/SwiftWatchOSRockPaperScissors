0ab985dbc51214823b7b90d2a1078bddbff02a52
Andres <andres_lopez_rm@hotmail.com>
2019-02-08 12:35:10 -0600

    Changes to Enum

--- RockPaperScissorsSwift WatchKit Extension/InterfaceController.swift
+++ RockPaperScissorsSwift WatchKit Extension/InterfaceController.swift
@@ -10,6 +10,19 @@ import WatchKit
 import Foundation
 
 
+enum Moves: String {
+    case rock
+    case paper
+    case scissors
+}
+
+enum Results: String {
+    case tie = "TIE"
+    case win = "WIN"
+    case lost = "LOST"
+}
+
+
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var oponentLabel: WKInterfaceLabel!
@@ -21,55 +34,59 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var paperButton: WKInterfaceButton!
     @IBOutlet weak var scissorsButton: WKInterfaceButton!
     
-    let options = ["rock", "paper", "scissors"]
+    let options = [Moves.rock, Moves.paper, Moves.scissors]
     var counter = 0
+    
+    
     
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        rockButton.setBackgroundImage(UIImage(named: "rock"))
-        paperButton.setBackgroundImage(UIImage(named: "paper"))
-        scissorsButton.setBackgroundImage(UIImage(named: "scissors"))
+        
+        
+        rockButton.setBackgroundImage(UIImage(named: Moves.rock.rawValue))
+        paperButton.setBackgroundImage(UIImage(named: Moves.paper.rawValue))
+        scissorsButton.setBackgroundImage(UIImage(named: Moves.scissors.rawValue))
         
         timer.start()
     }
     
     @IBAction func rock() {
-        check("rock")
+        check(Moves.rock)
     }
     
     @IBAction func paper() {
-        check("paper")
+        check(Moves.paper)
     }
     
     @IBAction func scissors() {
-        check("scissors")
+        check(Moves.scissors)
     }
     
-    func check(_ selection: String) {
+    func check(_ selection: Moves) {
         
         let ramdomOptions = options.shuffled()
         let machineChoose = ramdomOptions[0]
         
         if (selection == machineChoose) {
-            resultLabel.setText("TIE")
+            resultLabel.setText(Results.tie.rawValue)
             
-        } else if (selection == "rock" && machineChoose == "scissors"){
-            resultLabel.setText("WIN")
+        } else if (selection == .rock && machineChoose == .scissors){
+            resultLabel.setText(Results.win.rawValue.uppercased())
             counter += 1
-        } else if (selection == "rock" && machineChoose == "paper") {
-            resultLabel.setText("LOSE")
-        } else if (selection == "paper" && machineChoose == "rock") {
-            resultLabel.setText("WIN")
+        } else if (selection == .rock && machineChoose == .paper) {
+            resultLabel.setText(Results.lost.rawValue)
+        } else if (selection == .paper && machineChoose == .rock) {
+            resultLabel.setText(Results.win.rawValue)
             counter += 1
-        } else if (selection == "paper" && machineChoose == "scissors") {
-            resultLabel.setText("LOSE")
-        } else if (selection == "scissors" && machineChoose == "paper") {
-            resultLabel.setText("WIN")
+        } else if (selection == .paper && machineChoose == .scissors) {
+            resultLabel.setText(Results.lost.rawValue)
+        } else if (selection == .scissors && machineChoose == .paper) {
+            resultLabel.setText(Results.win.rawValue)
             counter += 1
-        } else if (selection == "scissors" && machineChoose == "rock") {
-            resultLabel.setText("LOSE")
+        } else if (selection == .scissors && machineChoose == .rock) {
+            resultLabel.setText(Results.lost.rawValue)
         }
         
         counterLabel.setText("Wins: \(counter)")
