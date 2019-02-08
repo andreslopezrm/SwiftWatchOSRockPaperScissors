7e6e85adfa9e7f88748449899ae7d73769599bae
Jason R Tibbetts <jason@tibbetts.net>
2019-02-08 11:35:02 -0500

    Moved hard-coded strings "WIN", "LOSE", and "TIE" into a Result enum. Moved the "rock", "paper", "scissors" sings into an Option enum. Replaced the if/else conditions in InterfaceController.check() with a switch. Correct a couple misspellings. (I understand the the developer is working in Spanish, but user-visible strings should be in English.)

--- RockPaperScissorsSwift WatchKit Extension/InterfaceController.swift
+++ RockPaperScissorsSwift WatchKit Extension/InterfaceController.swift
@@ -12,7 +12,7 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
-    @IBOutlet weak var oponentLabel: WKInterfaceLabel!
+    @IBOutlet weak var opponentLabel: WKInterfaceLabel!
     @IBOutlet weak var resultLabel: WKInterfaceLabel!
     @IBOutlet weak var counterLabel: WKInterfaceLabel!
     @IBOutlet weak var timer: WKInterfaceTimer!
@@ -21,68 +21,77 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var paperButton: WKInterfaceButton!
     @IBOutlet weak var scissorsButton: WKInterfaceButton!
     
-    let options = ["rock", "paper", "scissors"]
-    var counter = 0
+    enum Option: String {
+        case rock
+        case paper
+        case scissors
+        
+        var image: UIImage? {
+            return UIImage(named: rawValue)
+        }
+    }
     
+    enum Result: String {
+        case win = "WIN"
+        case tie = "TIE"
+        case lose = "LOSE"
+        
+        func update(label: WKInterfaceLabel) {
+            label.setText(rawValue)
+        }
+    }
+    
+    let options: [Option] = [.rock, .paper, .scissors]
+    var counter = 0
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        rockButton.setBackgroundImage(UIImage(named: "rock"))
-        paperButton.setBackgroundImage(UIImage(named: "paper"))
-        scissorsButton.setBackgroundImage(UIImage(named: "scissors"))
+        rockButton.setBackgroundImage(Option.rock.image)
+        paperButton.setBackgroundImage(Option.paper.image)
+        scissorsButton.setBackgroundImage(Option.scissors.image)
         
         timer.start()
     }
     
     @IBAction func rock() {
-        check("rock")
+        check(.rock)
     }
     
     @IBAction func paper() {
-        check("paper")
+        check(.paper)
     }
     
     @IBAction func scissors() {
-        check("scissors")
+        check(.scissors)
     }
     
-    func check(_ selection: String) {
+    func check(_ selection: Option) {
         
-        let ramdomOptions = options.shuffled()
-        let machineChoose = ramdomOptions[0]
+        let randomOptions = options.shuffled()
+        let machineChoose = randomOptions[0]
+        let result: Result
         
-        if (selection == machineChoose) {
-            resultLabel.setText("TIE")
-            
-        } else if (selection == "rock" && machineChoose == "scissors"){
-            resultLabel.setText("WIN")
-            counter += 1
-        } else if (selection == "rock" && machineChoose == "paper") {
-            resultLabel.setText("LOSE")
-        } else if (selection == "paper" && machineChoose == "rock") {
-            resultLabel.setText("WIN")
-            counter += 1
-        } else if (selection == "paper" && machineChoose == "scissors") {
-            resultLabel.setText("LOSE")
-        } else if (selection == "scissors" && machineChoose == "paper") {
-            resultLabel.setText("WIN")
-            counter += 1
-        } else if (selection == "scissors" && machineChoose == "rock") {
-            resultLabel.setText("LOSE")
+        switch (selection, machineChoose) {
+        case (.rock, .scissors),
+             (.paper, .rock),
+             (.scissors, .paper):
+            result = .win
+        case (.rock, .paper),
+             (.paper, .scissors),
+             (.scissors, .rock):
+            result = .lose
+        default:
+            result = .tie
         }
-        
+            
+        if result == .win {
+            counter += 1
+        }
+            
+        result.update(label: resultLabel)
         counterLabel.setText("Wins: \(counter)")
-        oponentLabel.setText("Oponent: \(machineChoose)")
-    }
-    
-    
-    override func willActivate() {
-        super.willActivate()
-    }
-    
-    override func didDeactivate() {
-        super.didDeactivate()
+        opponentLabel.setText("Opponent: \(machineChoose)")
     }
 
 }
